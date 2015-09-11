0606b2a6edb87c4df418a99d7fa988e6948ac409
Jebraith <jebraith@gmail.com>
2015-09-11 17:22:05 -0400

    Add R code and graphs - Exploratory Data Analysis

---
+++ Plot1.R
@@ -0,0 +1,37 @@
+
+
+##************************************************************************************
+## Created by: Joan Braithwaite
+## Date: 9/11/2015
+## Project: Exploratory Data Analysis, Assignment 1
+##
+## This assignment uses data from the UC Irvine Machine Learning Repository, a popular repository for machine learning datasets.
+##
+## Input: household_power_consumption text file
+##************************************************************************************ 
+## Read in data table
+
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+power <- read.table(file, header=T, sep=";")  ##Read text input file from working folder
+## Convert date to get subset
+power$Date <- as.Date(power$Date, format="%d/%m/%Y") ## Convert date
+## Select working set of data
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),] 
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+## merge date and time
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+## Generate Plot 1
+hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,54 @@
+##************************************************************************************
+## Created by: Joan Braithwaite
+## Date: 9/11/2015
+## Project: Exploratory Data Analysis, Assignment 1
+##
+## This assignment uses data from the UC Irvine Machine Learning Repository, a popular repository for machine learning datasets.
+##
+## Input: household_power_consumption text file
+##************************************************************************************ 
+## Read in data table
+
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+power <- read.table(file, header=T, sep=";")  ##Read text input file from working folder
+## Convert date to get subset
+power$Date <- as.Date(power$Date, format="%d/%m/%Y") ## Convert date
+## Select working set of data
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),] 
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+## merge date and time
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+## Generate Plot4
+par(mfrow=c(2,2))
+
+## upper left
+plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+## upper right
+plot(df$timestamp,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+## lower left
+plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(df$timestamp,df$Sub_metering_2,col="red")
+lines(df$timestamp,df$Sub_metering_3,col="blue")
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
+
+## lower right
+plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+#OUTPUT
+dev.copy(png, file="plot4.png", width=480, height=480)
+dev.off()
+
+
+
